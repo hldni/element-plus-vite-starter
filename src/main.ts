5a3ab68683379675af4b683b589398500333083c
hldni <1427213369@qq.com>
2023-09-17 14:38:00 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,12 @@ import "uno.css";
 // If you want to use ElMessage, import it.
 import "element-plus/theme-chalk/src/message.scss";
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App);
 // app.use(ElementPlus);
 app.mount("#app");
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
